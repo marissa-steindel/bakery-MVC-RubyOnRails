4d991d4608c83d78b2722413400d2751a9cf3e72
Marissa Steindel <marissa.steindel@gmail.com>
2023-04-08 21:22:26 -0500

    add cheet sheet to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+  root "provinces#index"
   resources :provinces
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
+
+
+  # GET	      /photos	          photos#index	  display a list of all photos
+  # POST	    /photos	          photos#create	  create a new photo
+  # GET	      /photos/new	      photos#new	    return an HTML form for creating a new photo
+  # GET	      /photos/:id	      photos#show	    display a specific photo
+  # PATCH/PUT	/photos/:id	      photos#update	  update a specific photo
+  # DELETE	  /photos/:id	      photos#destroy	delete a specific photo
+  # GET	      /photos/:id/edit	photos#edit	    return an HTML form for editing a photo
+
+  # photos_path           returns /photos
+  # new_photo_path        returns /photos/new
+  # edit_photo_path(:id)  returns /photos/:id/edit (edit_photo_path(10) returns /photos/10/edit)
+  # photo_path(:id)       returns /photos/:id (photo_path(10) returns /photos/10)

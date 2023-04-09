51e88aa0f053a3603661b41a8fc9ba791e8b4411
Marissa Steindel <marissa.steindel@gmail.com>
2023-04-08 22:35:07 -0500

    seed customers and provinces to the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,87 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "csv"
+
+# Destroy all database content
+# Customer.destroy_all
+# Province.destroy_all
+# Product.destroy_all
+# Category.destroy_all
+# AdminUser.destroy_all
+
+# SEED SALES TAX
+# # salestax_csv_file = Rails.root.join("db/salestax.csv")
+# # salestax_csv_data = File.read(csv_file)
+# # salestax_csv_data = CSV.parse(csv_data, headers: true)
+
+salestax_csv_data =  CSV.parse(File.read(Rails.root.join("db/salestax.csv")), headers: true)
+
+salestax_csv_data.each do |row|
+  puts "#{row["province"]}, #{row["code"]}"
+  puts "GST: #{row["GST"]}"
+  puts "HST: #{row["PST"]}"
+  puts "PST: #{row["HST"]}"
+  puts
+end
+
+
+salestax_csv_data.each do |row|
+  province = Province.create(  name:   row["province"],
+                                code:   row["code"],
+                                PST:    row["PST"],
+                                GST:    row["GST"],
+                                HST:    row["HST"]
+                            )
+end
+
+# Province.all.each do |prov|
+#   puts "Name: #{prov.name}"
+#   puts "Code: #{prov.code}"
+#   puts "GST: #{prov.GST}"
+#   puts "HST: #{prov.HST}"
+#   puts "PST: #{prov.PST}"
+#   puts
+# end
+
+# salestax_csv_data.each do |row|
+#   province = Province.create(
+#     name:   row["province"],
+#     code:   row["code"],
+#     PST:    row["PST"],
+#     GST:    row["GST"],
+#     HST:    row["HST"]
+#   )
+
+
+
+# SEED customers and province simultaneously
+# salestax_csv_data.each do |row|
+#   province = Province.create(
+#     name:   row["province"],
+#     code:   row["code"],
+#     PST:    row["PST"],
+#     GST:    row["GST"],
+#     HST:    row["HST"]
+#   )
+#   rand(1..3).times do
+#     customer = Customer.create(
+#       name: Faker::Name.name,
+#       address: Faker::Address.street_address,
+#       username: Faker::Lorem.word,
+#       password: Faker::Internet.password,
+#       province: province
+#     )
+#     puts "#{customer.name} from #{province.name} created"
+#   end
+#   # puts seed.inspect
+# end
+
+
+30.times do
+  customer = Customer.create(
+    name: Faker::Name.name,
+    address: Faker::Address.street_address,
+    username: Faker::Lorem.word,
+    password_digest: Faker::Internet.password,
+    province: Province.find(rand(1..13))
+  )
+  puts "#{customer.name} from #{customer.province.name} created"
+end

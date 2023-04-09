3e6c6a5f168adc43fd094c85a8d6d64249e38488
Marissa Steindel <marissa.steindel@gmail.com>
2023-04-09 00:28:29 -0500

    migrate join table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_09_050758) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_09_052515) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "category_id", null: false
   end
 
   create_table "customers", force: :cascade do |t|

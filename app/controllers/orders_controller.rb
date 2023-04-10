1aaadac1cee9af6d54c5362002ad4caa4313d070
Marissa Steindel <marissa.steindel@gmail.com>
2023-04-10 18:11:11 -0500

    remove json formatting

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -23,38 +23,28 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
 
-    respond_to do |format|
       if @order.save
-        format.html { redirect_to order_url(@order), notice: "Order was successfully created." }
-        format.json { render :show, status: :created, location: @order }
+        redirect_to order_url(@order), notice: "Order was successfully created."
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity
       end
-    end
   end
 
   # PATCH/PUT /orders/1 or /orders/1.json
   def update
-    respond_to do |format|
       if @order.update(order_params)
-        format.html { redirect_to order_url(@order), notice: "Order was successfully updated." }
-        format.json { render :show, status: :ok, location: @order }
+        redirect_to order_url(@order), notice: "Order was successfully updated."
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        render :edit, status: :unprocessable_entity
       end
-    end
   end
 
   # DELETE /orders/1 or /orders/1.json
   def destroy
     @order.destroy
 
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
-      format.json { head :no_content }
-    end
+      redirect_to orders_url, notice: "Order was successfully destroyed."
+      head :no_content
   end
 
   private
